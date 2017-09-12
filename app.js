df4484c60aedacde6acb7824d648b4c1eb929819
KubbaQ <jakonn@gmail.com>
2017-09-12 22:18:34 +0200

    added mocha, simple tests and makefile for testing

--- app.js
+++ app.js
@@ -13,6 +13,20 @@ app.all("*", function(req, res) {
   res.render("index", { msg: "Welcome to the Practical Node.js!" });
 });
 
-http.createServer(app).listen(app.get("port"), function() {
-  console.log("Express.js server listening on port " + app.get("port"));
-});
+var server = http.createServer(app);
+var boot = function() {
+  server.listen(app.get("port"), function() {
+    console.info("Express server listening on port " + app.get("port"));
+  });
+};
+var shutdown = function() {
+  server.close();
+};
+if (require.main === module) {
+  boot();
+} else {
+  console.info("Running app as a module");
+  exports.boot = boot;
+  exports.shutdown = shutdown;
+  exports.port = app.get("port");
+}
